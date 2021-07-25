86b54dc529a9f6163aec5e596a7c2cc2167c4e10
Archit Chandra <architchandra@gmail.com>
2021-07-25 23:30:01 +0530

    Set up home page routing

--- src/app.js
+++ src/app.js
@@ -1,8 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route } from 'react-router-dom';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 
-ReactDOM.render(<p>Expensify App</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => (
+  <h1>Expense Dashboard Page</h1>
+);
+
+const routes = (
+  <BrowserRouter>
+    <div>
+      <Route path="/" component={ ExpenseDashboardPage } />
+    </div>
+  </BrowserRouter>
+);
+ReactDOM.render(routes, document.getElementById('app'));
