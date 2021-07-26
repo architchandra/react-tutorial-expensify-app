ef6f85c79b0016af86255c76c51ebd93830ed999
Archit Chandra <architchandra@gmail.com>
2021-07-26 17:27:13 +0530

    Move components and routers into separate files

--- src/app.js
+++ src/app.js
@@ -1,66 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+import AppRouter from './routers/AppRouter';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
 
-const ExpenseDashboardPage = () => (
-  <h2>Expense Dashboard Page</h2>
-);
-
-const AddExpensePage = () => (
-  <h2>Add an Expense</h2>
-);
-
-const EditExpensePage = () => (
-  <h2>Edit an Expense</h2>
-);
-
-const HelpPage = () => (
-  <h2>Help</h2>
-);
-
-const NotFoundPage = () => (
-  <div>
-    <h2>Page not found</h2>
-    <p>Go to the <Link to="/">dashboard</Link>.</p>
-  </div>
-);
-
-const Header = () => (
-  <header>
-    <h1>Expensify</h1>
-    <ul>
-      <li>
-        <NavLink to="/" activeClassName="navitem-active" exact={ true }>Dashboard</NavLink>
-      </li>
-      <li>
-        <NavLink to="/create" activeClassName="navitem-active">Create</NavLink>
-      </li>
-      <li>
-        <NavLink to="/edit" activeClassName="navitem-active">Edit</NavLink>
-      </li>
-      <li>
-        <NavLink to="/help" activeClassName="navitem-active">Help</NavLink>
-      </li>
-    </ul>
-  </header>
-);
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route path="/" component={ ExpenseDashboardPage } exact={ true } />
-        <Route path="/create" component={ AddExpensePage } />
-        <Route path="/edit" component={ EditExpensePage } />
-        <Route path="/help" component={ HelpPage } />
-        <Route component={ NotFoundPage } />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-ReactDOM.render(routes, document.getElementById('app'));
+ReactDOM.render(<AppRouter />, document.getElementById('app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+
+const EditExpensePage = () => (
+  <h2>Edit an Expense</h2>
+);
+export default EditExpensePage;
