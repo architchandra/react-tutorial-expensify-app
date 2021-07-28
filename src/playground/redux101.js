1ca3b17f4ccd8a2c999ed9761d6c8d3addf0b5cc
Archit Chandra <architchandra@gmail.com>
2021-07-28 10:26:07 +0530

    Explore dynamic actions

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -5,12 +5,18 @@ import { createStore } from 'redux';
 const store = createStore((state = { count:  0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1,
+        count: state.count + incrementBy,
       };
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1,
+        count: state.count - decrementBy,
+      };
+    case 'SET':
+      return {
+        count: 101,
       };
     case 'RESET':
       return {
@@ -23,8 +29,17 @@ const store = createStore((state = { count:  0 }, action) => {
 });
 
 
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
 store.dispatch({
   type: 'INCREMENT',
+});
+store.dispatch({
+  type: 'INCREMENT',
+  incrementBy: 5,
 });
 store.dispatch({
   type: 'RESET',
@@ -32,4 +47,12 @@ store.dispatch({
 store.dispatch({
   type: 'DECREMENT',
 });
-console.log(store.getState());
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10,
+});
+store.dispatch({
+  type: 'SET',
+  count: 101,
+});
+
