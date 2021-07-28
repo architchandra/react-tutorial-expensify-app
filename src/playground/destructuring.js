b8c8ce99cdf9832b5ae79cd9d5c3b1442e88b8d9
Archit Chandra <architchandra@gmail.com>
2021-07-28 15:19:17 +0530

    Explore array destructuring syntax

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -1,10 +1,17 @@
-const book = {
-  title: 'Ego is the Enemy',
-  author: 'Ryan Holiday',
-  publisher: {
-    name: 'Penguin',
-  },
-};
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     name: 'Penguin',
+//   },
+// };
 
-const { name: publisherName = 'Self Published'} = book.publisher;
-console.log(publisherName);
+// const { name: publisherName = 'Self Published'} = book.publisher;
+// console.log(publisherName);
+
+
+
+const item = ['Coffee', 'INR 20', 'INR 30', 'INR 40'];
+const [itemName, priceSmall, priceMedium, priceLarge] = item;
+
+console.log(`A medium ${itemName} costs ${priceMedium}.`);
