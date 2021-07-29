230db4b5202db4dc7d6fda56cbd5d7fdc7046abd
Archit Chandra <architchandra@gmail.com>
2021-07-29 10:29:30 +0530

    Create action generators for dispatch calls

--- src/playground/redux101.js
+++ src/playground/redux101.js
@@ -2,21 +2,39 @@ import { createStore } from 'redux';
 
 
 
+// If function is called without args, it defaults to an empty object.
+// The empty object uses the destructured property's default value of 1.
+const incrementCount = ({ incrementBy = 1 } = {}) => ({
+  type: 'INCREMENT',
+  incrementBy,
+});
+const decrementCount = ({ decrementBy = 1 } = {}) => ({
+  type: 'DECREMENT',
+  decrementBy,
+});
+const setSount = ({ count }) => ({
+  type: 'SET',
+  count,
+});
+const resetCount = () => ({
+  type: 'RESET',
+});
+
+
+
 const store = createStore((state = { count:  0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + incrementBy,
+        count: state.count + action.incrementBy,
       };
     case 'DECREMENT':
-      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - decrementBy,
+        count: state.count - action.decrementBy,
       };
     case 'SET':
       return {
-        count: 101,
+        count: action.count,
       };
     case 'RESET':
       return {
@@ -34,25 +52,10 @@ store.subscribe(() => {
   console.log(store.getState());
 });
 
-store.dispatch({
-  type: 'INCREMENT',
-});
-store.dispatch({
-  type: 'INCREMENT',
-  incrementBy: 5,
-});
-store.dispatch({
-  type: 'RESET',
-});
-store.dispatch({
-  type: 'DECREMENT',
-});
-store.dispatch({
-  type: 'DECREMENT',
-  decrementBy: 10,
-});
-store.dispatch({
-  type: 'SET',
-  count: 101,
-});
+store.dispatch(incrementCount());
+store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(resetCount());
+store.dispatch(decrementCount());
+store.dispatch(decrementCount({ decrementBy: 10 }));
+store.dispatch(setSount({ count: 101 }));
 
