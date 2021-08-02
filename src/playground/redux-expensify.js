4a1b4daba6c2b0482a24b8086ca8d3a10c8b7851
Archit Chandra <architchandra@gmail.com>
2021-08-02 14:04:12 +0530

    Create expenses and filters reducers

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -2,6 +2,35 @@ import { createStore, combineReducers } from 'redux';
 
 
 
+const expensesReducerDefaultState = [];
+const expensesReducer = (state = expensesReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+const filtersReducerDefaultState = {
+  text: '',
+  sortBy: 'date',
+  stateDate: undefined,
+  endDate: undefined,
+};
+const filtersReducer = (state = filtersReducerDefaultState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+const store = createStore(
+  combineReducers({
+    expenses: expensesReducer,
+    filters: filtersReducer,
+  })
+);
+
+
+
 const demoState = {
   expenses: [{
     id: 'jnjkdscnjdnv',
