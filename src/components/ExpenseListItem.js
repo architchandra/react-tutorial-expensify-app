214b0c2742a48cdb952b665bdddefd7361f578a2
Archit Chandra <architchandra@gmail.com>
2021-08-18 14:38:32 +0530

    Allow individual expenses to be removed

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
 
 
-const ExpenseListItem = ({id, description, note, amount, createdAt}) => (
+const ExpenseListItem = ({id, description, note, amount, createdAt, dispatch}) => (
   <li>
     {description && <p>Description: {description}</p>}
     {amount && <p>Amount: {amount}</p>}
     {!!(createdAt) && <p>Created at: {createdAt}</p>}
+    <button type="button" onClick={(e) => {
+      dispatch(removeExpense({ id }));
+    }}>
+      Remove
+    </button>
   </li>
 );
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
