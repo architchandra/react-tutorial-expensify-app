f282a6c40a7839baece2d0582fa9d3d45ccb0d04
Archit Chandra <architchandra@gmail.com>
2021-08-18 14:14:51 +0530

    Allow expenses to be filtered by text

--- src/components/ExpenseDashboardPage.js
+++ src/components/ExpenseDashboardPage.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ExpenseList from './ExpenseList';
+import ExpenseFiltersList from './ExpenseFiltersList';
 
 
 
 const ExpenseDashboardPage = () => (
   <div>
     <h2>Expense Dashboard Page</h2>
+    <ExpenseFiltersList />
     <ExpenseList />
   </div>
 );
