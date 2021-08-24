7c671e2ea47c4b8cb69e00a105d62caa63d75a12
Archit Chandra <architchandra@gmail.com>
2021-08-24 13:31:23 +0530

    Improve format of created at time

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import moment from 'moment';
 
 
 
-const ExpenseListItem = ({id, description, note, amount, createdAt}) => (
+const ExpenseListItem = ({id, description, note, amount, createdAt}) => {
+  return (
   <li>
     <Link to={`/edit/${id}`}>
       {description && <p>Description: {description}</p>}
     </Link>
     {amount && <p>Amount: {amount}</p>}
-    {!!(createdAt) && <p>Created at: {createdAt}</p>}
+    {!!(createdAt) && <p>Created at: {moment(createdAt).format('Do MMMM YYYY, h:mm a')}</p>}
   </li>
-);
+)};
 export default ExpenseListItem;
