f5279b374a4e495c9076d740f1baa1f073c46cf9
Archit Chandra <architchandra@gmail.com>
2022-02-01 18:03:24 +0530

    Use Enzyme for snapshot testing of Header component

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import ReactShallowRenderer from 'react-test-renderer/shallow';
+import { shallow } from 'enzyme';
+import toJson from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 
 
 
 test('should render Header correctly', () => {
-  const renderer = new ReactShallowRenderer();
-  renderer.render(<Header />);
-  expect(renderer.getRenderOutput()).toMatchSnapshot();
+  const wrapper = shallow(<Header />);
+  expect(toJson(wrapper)).toMatchSnapshot();
 });
