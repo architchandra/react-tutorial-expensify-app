9605241e23efa3213c3b49ca451daddce51c1062
Archit Chandra <architchandra@gmail.com>
2022-02-01 18:16:03 +0530

    Move enzyme to json serializer to Jest config file

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import toJson from 'enzyme-to-json';
 import Header from '../../components/Header';
 
 
@@ -8,5 +7,5 @@ import Header from '../../components/Header';
 
 test('should render Header correctly', () => {
   const wrapper = shallow(<Header />);
-  expect(toJson(wrapper)).toMatchSnapshot();
+  expect(wrapper).toMatchSnapshot();
 });
