2eb5bc89d2d080fd2c01313c36f8e27e5ed62b97
Archit Chandra <architchandra@gmail.com>
2022-02-02 20:26:30 +0530

    Refactor Add Expense Page tests to be more DRY

--- src/tests/components/AddExpensePage.test.js
+++ src/tests/components/AddExpensePage.test.js
@@ -5,17 +5,20 @@ import expenses from '../fixtures/expenses';
 
 
 
+let onSubmitSpy, historySpy, wrapper;
+beforeEach(() => {
+  onSubmitSpy = jest.fn();
+  historySpy = { push: jest.fn() };
+  wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={historySpy} />);
+});
+
+
+
 test('should render Add Expense page', () => {
-  const onSubmitSpy = jest.fn();
-  const history = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={history} />);
   expect(wrapper).toMatchSnapshot();
 });
 
 test('should handle onSubmit', () => {
-  const onSubmitSpy = jest.fn();
-  const historySpy = { push: jest.fn() };
-  const wrapper = shallow(<AddExpensePage onSubmit={onSubmitSpy} history={historySpy} />);
   wrapper.find('ExpenseForm').prop('onSubmit')(expenses[0]);
   expect(historySpy.push).toHaveBeenLastCalledWith('/');
   expect(onSubmitSpy).toHaveBeenLastCalledWith(expenses[0]);
