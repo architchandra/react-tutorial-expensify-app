e5243a599da3919ac5eafef141c001f145ab18ec
Archit Chandra <architchandra@gmail.com>
2022-02-07 19:55:59 +0530

    Add source maps to dev build

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,18 @@ module.exports = (env) => {
         test: /\.s?css$/,
         use: CssExtract.extract({
           use: [
-            'css-loader',
-            'sass-loader',
+            {
+              loader: 'css-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
+            {
+              loader: 'sass-loader',
+              options: {
+                sourceMap: true,
+              },
+            },
           ],
         }),
       }],
@@ -35,7 +45,7 @@ module.exports = (env) => {
     plugins: [
       CssExtract,
     ],
-    devtool: isProduction ? 'source-map' : 'cheap-module-eval-source-map',
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
