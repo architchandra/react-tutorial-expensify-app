c12652b30cded445a1a87f212503d7aff4e54bdc
Archit Chandra <architchandra@gmail.com>
2022-02-08 16:29:35 +0530

    Move build CSS and JS files to dist directory

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js',
     },
     module: {
@@ -49,6 +49,7 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
+      publicPath: '/dist/',
     },
   };
 };
