f1c058f9de889a77dc30101f5d3aa9cfa1b48afe
Archit Chandra <architchandra@gmail.com>
2022-02-08 13:54:35 +0530

    Set up server and scripts for running on Heroku

--- server/server.js
+++ server/server.js
@@ -5,11 +5,12 @@ const express = require('express');
 
 const app = express();
 const publicPath = path.join(__dirname, '..', 'public');
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
 app.get('*', (request, response) => {
   response.sendFile(path.join(publicPath, 'index.html'));
 });
-app.listen(3000, () => {
+app.listen(port, () => {
   console.log('Server is up!');
 });
