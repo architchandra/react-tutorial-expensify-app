7e5c73f2446c57f43f3559e1a5c7e731d4415bc7
Archit Chandra <architchandra@gmail.com>
2022-02-28 20:37:59 +0530

    Set up Redux Thunk - first pass

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,11 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import expensesReducer from '../reducers/expenses';
 import filtersReducer from '../reducers/filters';
+import thunk from 'redux-thunk';
 
 
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const configureStore = () => (
   createStore(
@@ -10,7 +13,7 @@ const configureStore = () => (
       expenses: expensesReducer,
       filters: filtersReducer,
     }),
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    composeEnhancers(applyMiddleware(thunk))
   )
 );
 
