8aa22ed7c8b0501b3e82a370cf6d47f70de29e56
Archit Chandra <architchandra@gmail.com>
2022-02-28 13:45:46 +0530

    Remove unused imports from app.js file

--- src/app.js
+++ src/app.js
@@ -1,12 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import moment from 'moment';
 import AppRouter from './routers/AppRouter';
 import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getExpenses from './selectors/expenses';
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
