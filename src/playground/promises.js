339e5f5d511a8710418e6d4e257be3ff820200b0
Archit Chandra <architchandra@gmail.com>
2022-03-03 16:22:55 +0530

    Add promise chaining example

--- src/playground/promises.js
+++ src/playground/promises.js
@@ -1,14 +1,31 @@
 const promise = new Promise((resolve, reject) => {
   setTimeout(() => {
-    // resolve('This is my resolved data');
-    reject('Something went wrong');
+    resolve('This is my resolved data');
+    // reject('Something went wrong');
   }, 1500)
 });
+
+// console.log('before');
+
+// promise.then((data) => {
+//   console.log(data);
+// }).catch((error) => {
+//   console.log('error:', error);
+// });
+
+// console.log('after');
+
+
+
+// Promise chaining
 
 console.log('before');
 
 promise.then((data) => {
   console.log(data);
+  return 'yoyoyo';
+}).then((yo) => {
+  console.log('msg', yo);
 }).catch((error) => {
   console.log('error:', error);
 });
