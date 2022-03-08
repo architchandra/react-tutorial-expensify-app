5cce245549fb9019b979b14316d96a7586fa18d5
Archit Chandra <architchandra@gmail.com>
2022-03-08 20:05:34 +0530

    Update tests for Header component

--- src/tests/components/Header.test.js
+++ src/tests/components/Header.test.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import Header from '../../components/Header';
+import { Header } from '../../components/Header';
 
+
+
+let startLogoutSpy, wrapper;
+beforeEach(() => {
+  startLogoutSpy = jest.fn();
+  wrapper = shallow(<Header startLogout={startLogoutSpy} />)
+});
 
 
 
 test('should render Header correctly', () => {
-  const wrapper = shallow(<Header />);
   expect(wrapper).toMatchSnapshot();
+});
+
+test('should call startLogout on button click', () => {
+  wrapper.find('button').simulate('click');
+  expect(startLogoutSpy).toHaveBeenCalled();
 });
